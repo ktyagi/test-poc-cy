3616b94236836351e4fdabd099dad27f2c1fa68e
kuldeep kumar <kuldeeptyagi83@gmail.com>
2019-04-26 07:34:56 +1200

    UPDATED CYPRESS.JSON FOR USERNAME AND PASS

--- cypress/integration/common/beforeEach.js
+++ cypress/integration/common/beforeEach.js
@@ -1,7 +1,7 @@
 
 beforeEach(() => {
   cy.visit('/')
-    .login('enter username', 'enter password')
+    .login(Cypress.env("USERNAME"), Cypress.env("PASSWORD"))
     .log(
       "This will run before every scenario."
     );
